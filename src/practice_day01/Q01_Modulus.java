0e24ef16d92ef79aa9fd065e3106fd12d0d7cef9
Adem <ademk760@gmail.com>
2022-08-20 15:29:40 +0200

    version2

--- src/practice_day01/Q01_Modulus.java
+++ src/practice_day01/Q01_Modulus.java
@@ -6,7 +6,7 @@ public class Q01_Modulus {
     public static void main(String[] args) {
         /*
          *  Kullanicidanalacaginiz 5 basamakli sayinin ilk 2 ve son iki basamagindaki rakamlari toplamini bulunuz
-         *
+         *  aa
          *   Ex :
          *   input  : 12345
          *   output : 12
