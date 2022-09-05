c693127b754a666fe570f565cad5524ea0264f70
Adem <ademk760@gmail.com>
2022-09-05 13:26:38 +0200

    Revert "Update Q01_Modulus.java"
    This reverts commit 3725f9e67954202d4ee1756b5c6b9218c46c58de.

--- src/practice_java/practice_day01/Q01_Modulus.java
+++ src/practice_java/practice_day01/Q01_Modulus.java
@@ -6,7 +6,7 @@ public class Q01_Modulus {
     public static void main(String[] args) {
         /*
          *  Kullanicidanalacaginiz 5 basamakli sayinin ilk 2 ve son iki basamagindaki rakamlari toplamini bulunuz
-         *  aa bu not githup dan eklendi
+         *  aa
          *   Ex :
          *   input  : 12345
          *   output : 12
