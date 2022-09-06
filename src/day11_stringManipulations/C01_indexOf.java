ff20028a97f2b73711bcaef55312b1d5da4e84fe
Adem <ademk760@gmail.com>
2022-09-06 15:07:12 +0200

    ab

--- src/day11_stringManipulations/C01_indexOf.java
+++ src/day11_stringManipulations/C01_indexOf.java
@@ -10,6 +10,7 @@ public class C01_indexOf {
         System.out.println(str.indexOf("r")); //7
         System.out.println(str.indexOf("j")); // -1
         System.out.println();
+        System.out.println();
         System.out.println(str.indexOf("mek")); // 10
 
         // eger istedigimiz index den sonrasini kontrol etnek istersek
